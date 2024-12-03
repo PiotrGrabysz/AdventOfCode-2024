64356d374647e5de00d5a8b626827eac55cc81b9
Piotr Grabysz <pgrabysz@yahoo.com>
2024-12-03 23:52:19 +0100

    refactor: move parsing multiplication instructions to separate function

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -15,9 +15,6 @@ xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 const TEST2: &str = "\
 xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
 ";
-// const TEST2: &str = "\
-// mul(2,4)don't()mul(1,3)don't()mul(11,33)do()mul(22,44)do()
-// ";
 fn main() -> Result<()> {
     start_day(DAY);
 
@@ -26,20 +23,10 @@ fn main() -> Result<()> {
 
     fn part1<R: BufRead>(reader: &mut R) -> Result<i32> {
         // Capture patterns like "mul(x,y)", where "x" and "y" are numbers up to three digits
-        let multiplication_formula_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
 
         let file_content = read_file_to_string(reader);
 
-        let multiplication_sum = multiplication_formula_regex
-            .captures_iter(file_content.as_str())
-            .filter_map(|captures| {
-                let x = captures.get(1)?.as_str().parse::<i32>().ok()?;
-                let y = captures.get(2)?.as_str().parse::<i32>().ok()?;
-                Some(x * y)
-            })
-            .sum();
-
-        Ok(multiplication_sum)
+        sum_mul_operations(file_content.as_str())
     }
 
     assert_eq!(161, part1(&mut BufReader::new(TEST.as_bytes()))?);
@@ -53,41 +40,36 @@ fn main() -> Result<()> {
     println!("\n=== Part 2 ===");
 
     fn part2<R: BufRead>(reader: &mut R) -> Result<i32> {
-        let disabled_operations_regex = Regex::new(r"don't\(\)")?;
-        let enabled_operations_regex = Regex::new(r"do\(\)")?;
-        let mul_regex = Regex::new(r"mul\(\d{1,3},\d{1,3}\)$")?;
-        let extract_numbers_regex = Regex::new(r"\d{1,3}")?;
+        let multiplication_formula_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
+        let disable_instruction_regex = Regex::new(r"don't\(\)")?;
+        let enable_instruction_regex = Regex::new(r"do\(\)")?;
 
-        let mut file_content: String = String::new();
-        _ = reader.read_to_string(&mut file_content);
+        let file_content = read_file_to_string(reader);
 
         let mut multiplication_sum = 0;
-
+        let mut disabled_instruction_mode = false;
         let mut current_string: String = String::new();
-        let mut disabled_operations = false;
         for char in file_content.chars() {
-            // dbg!(char);
             current_string.push(char);
-            // dbg!(&current_string);
-            if disabled_operations_regex.is_match(&current_string) {
-                disabled_operations = true;
+
+            if disable_instruction_regex.is_match(&current_string) {
+                disabled_instruction_mode = true;
                 current_string.clear();
                 continue;
             }
-            if enabled_operations_regex.is_match(&current_string) {
-                disabled_operations = false;
+
+            if enable_instruction_regex.is_match(&current_string) {
+                disabled_instruction_mode = false;
                 current_string.clear();
                 continue;
             }
-            if !disabled_operations {
-                if let Some(res) = mul_regex.find_iter(&current_string).last() {
-                    let numbers: Vec<i32> = extract_numbers_regex
-                        .find_iter(res.as_str())
-                        .filter_map(|m| m.as_str().parse::<i32>().ok())
-                        .collect();
-                    // dbg!("Adding {} * {}",  numbers[0], numbers[1]);
-                    multiplication_sum += numbers[0] * numbers[1];
-                    current_string.clear();
+            if !disabled_instruction_mode {
+                match get_last_multiplication(current_string.as_str(), &multiplication_formula_regex) {
+                    Some(m) => {
+                        multiplication_sum += m;
+                        current_string.clear();
+                    }
+                    None => continue,
                 }
             }
         }
@@ -108,4 +90,42 @@ fn read_file_to_string<R: BufRead>(reader: &mut R) -> String {
     let mut file_content: String = String::new();
     _ = reader.read_to_string(&mut file_content);
     file_content
+}
+
+fn sum_mul_operations(instructions: &str) -> Result<i32> {
+    let multiplication_formula_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
+
+    let multiplication_sum: i32 = multiplication_formula_regex
+        .captures_iter(instructions)
+        .filter_map(|captures| {
+            let x = captures.get(1)?.as_str().parse::<i32>().ok()?;
+            let y = captures.get(2)?.as_str().parse::<i32>().ok()?;
+            Some(x * y)
+        })
+        .sum();
+    Ok(multiplication_sum)
+}
+
+fn get_last_multiplication(instructions: &str, multiplication_formula_regex: &Regex) -> Option<i32> {
+    if let Some(captures) = multiplication_formula_regex
+        .captures_iter(instructions)
+        .last()
+    {
+        let num1 = captures
+            .get(1)
+            .unwrap()
+            .as_str()
+            .parse::<i32>()
+            .ok()
+            .unwrap();
+        let num2 = captures
+            .get(2)
+            .unwrap()
+            .as_str()
+            .parse::<i32>()
+            .ok()
+            .unwrap();
+        return Some(num1 * num2)
+    }
+    None
 }
