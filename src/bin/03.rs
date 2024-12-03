4fb819f5f37edc5afb4f7340c15fdc84c45448d3
Piotr Grabysz <pgrabysz@yahoo.com>
2024-12-03 11:53:15 +0100

    Day 3 first part

---
+++ src/bin/03.rs
@@ -0,0 +1,70 @@
+use adv_code_2024::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "03";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+";
+const TEST2: &str = "\
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+";
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<i32> {
+        let mul_regex = Regex::new(r"mul\(\d{1,3},\d{1,3}\)")?;
+        let extract_numbers_regex = Regex::new(r"\d{1,3}")?;
+
+        let lines = reader.lines().flatten().collect::<Vec<_>>();
+
+        let mut multiplication_sum = 0;
+        for line in &lines {
+            for cap in mul_regex.captures_iter(&line) {
+                let multiply_operation = &cap[0];
+
+                let numbers: Vec<i32> = extract_numbers_regex
+                    .find_iter(multiply_operation)
+                    .filter_map(|m| m.as_str().parse::<i32>().ok())
+                    .collect();
+
+                multiplication_sum += numbers[0] * numbers[1];
+            }
+        }
+
+        Ok(multiplication_sum)
+    }
+
+    // TODO: Set the expected answer for the test input
+    assert_eq!(161, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    // println!("\n=== Part 2 ===");
+    //
+    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    //     Ok(0)
+    // }
+    //
+    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    //
+    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    // let result = time_snippet!(part2(input_file)?);
+    // println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
