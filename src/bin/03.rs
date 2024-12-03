2a9d84070a95cab801603c79f8f63606e5fb5903
Piotr Grabysz <pgrabysz@yahoo.com>
2024-12-03 13:12:19 +0100

    Day 3 second part dirty solution

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -15,6 +15,9 @@ xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 const TEST2: &str = "\
 xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
 ";
+// const TEST2: &str = "\
+// mul(2,4)don't()mul(1,3)don't()mul(11,33)do()mul(22,44)do()
+// ";
 fn main() -> Result<()> {
     start_day(DAY);
 
@@ -53,17 +56,55 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: &mut R) -> Result<i32> {
+        let disabled_operations_regex = Regex::new(r"don't\(\)")?;
+        let enabled_operations_regex = Regex::new(r"do\(\)")?;
+        let mul_regex = Regex::new(r"mul\(\d{1,3},\d{1,3}\)$")?;
+        let extract_numbers_regex = Regex::new(r"\d{1,3}")?;
+
+        let mut file_content: String = String::new();
+        _ = reader.read_to_string(&mut file_content);
+
+        let mut multiplication_sum = 0;
+
+        let mut current_string: String = String::new();
+        let mut disabled_operations = false;
+        for char in file_content.chars() {
+            // dbg!(char);
+            current_string.push(char);
+            // dbg!(&current_string);
+            if disabled_operations_regex.is_match(&current_string) {
+                disabled_operations = true;
+                current_string.clear();
+                continue;
+            }
+            if enabled_operations_regex.is_match(&current_string) {
+                disabled_operations = false;
+                current_string.clear();
+                continue;
+            }
+            if !disabled_operations {
+                if let Some(res) = mul_regex.find_iter(&current_string).last() {
+                    let numbers: Vec<i32> = extract_numbers_regex
+                        .find_iter(res.as_str())
+                        .filter_map(|m| m.as_str().parse::<i32>().ok())
+                        .collect();
+                    // dbg!("Adding {} * {}",  numbers[0], numbers[1]);
+                    multiplication_sum += numbers[0] * numbers[1];
+                    current_string.clear();
+                }
+            }
+        }
+        Ok(multiplication_sum)
+    }
+
+    assert_eq!(48, part2(&mut BufReader::new(TEST2.as_bytes()))?);
+
+    let mut input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(&mut input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
