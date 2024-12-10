48d99f0314d128b37694cc23d0b0b25d4adbd059
Piotr Grabysz <pgrabysz@yahoo.com>
2024-12-10 21:46:06 +0100

    Day 10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,9 +1,9 @@
-use std::collections::HashSet;
 use adv_code_2024::board_matrix::{Board, Point};
 use adv_code_2024::start_day;
 use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -46,7 +46,6 @@ fn main() -> Result<()> {
                     &topographic_map,
                 );
                 counter += count_unique_trail_ends(trail_ends);
-
             }
         }
         Ok(counter)
@@ -60,17 +59,36 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let topographic_map = Board::<i8>::from_buffer(reader);
+
+        let mut counter = 0;
+        for row in 0..topographic_map.n_rows {
+            for col in 0..topographic_map.n_cols {
+                let value = topographic_map.get_value(row, col)?;
+                if *value != 0 {
+                    continue;
+                }
+                counter += traverse_trail_and_count_rating(
+                    Point {
+                        x: col as i32,
+                        y: row as i32,
+                    },
+                    -1,
+                    &topographic_map,
+                );
+            }
+        }
+        Ok(counter)
+    }
+
+    assert_eq!(81, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
@@ -102,7 +120,6 @@ fn traverse_trail(
         let next_position = current_position.add(direction);
         let trail_end = traverse_trail(next_position, *current_height, topographic_map);
         trail_ends.extend(trail_end);
-
     }
     trail_ends
 }
@@ -117,4 +134,34 @@ fn count_unique_trail_ends(trail_ends: Vec<Point>) -> usize {
         }
     }
     counter
+}
+
+fn traverse_trail_and_count_rating(
+    current_position: Point,
+    previous_height: i8,
+    topographic_map: &Board<i8>,
+) -> usize {
+    let current_height = match topographic_map.get_value_from_point(&current_position) {
+        Result::Ok(value) => value,
+        Err(_) => return 0,
+    };
+
+    if (current_height - previous_height) != 1 {
+        return 0;
+    }
+    if *current_height == 9 {
+        return 1;
+    }
+
+    let mut total = 0;
+    for direction in &[
+        Point { x: 1, y: 0 },
+        Point { x: -1, y: 0 },
+        Point { x: 0, y: 1 },
+        Point { x: 0, y: -1 },
+    ] {
+        let next_position = current_position.add(direction);
+        total += traverse_trail_and_count_rating(next_position, *current_height, topographic_map);
+    }
+    total
 }
